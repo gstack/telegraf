184c2c6c1a3974a693482c623264994604136699
Tanner Bruce <tanner.bruce@boldcommerce.ca>
2018-05-15 08:45:24 -0500

    make gitlab scraping async

--- plugins/inputs/gitlab/gitlab.go
+++ plugins/inputs/gitlab/gitlab.go
@@ -7,6 +7,8 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"context"
+	"sync"
 )
 
 const (
@@ -25,9 +27,13 @@ const (
 
 type Gitlab struct {
 	client *gitlab.Client
+	ctx context.Context
+	wg *sync.WaitGroup
+	cancel context.CancelFunc
+	currentPage int
+	projects map[int]string
 	Token string
 	Endpoint string
-	currentPage int
 }
 
 // Description - display description
@@ -38,78 +44,87 @@ func (h *Gitlab) SampleConfig() string { return sampleConfig }
 
 // Gather - Main code responsible for gathering, processing and creating metrics
 func (h *Gitlab) Gather(acc telegraf.Accumulator) error {
+	return nil
+}
+
+func (h *Gitlab) Stop() {
+	h.cancel()
+	h.wg.Wait()
+}
+
+func (h *Gitlab) Start(acc telegraf.Accumulator) error {
+	h.ctx, h.cancel = context.WithCancel(context.Background())
+	h.wg = &sync.WaitGroup{}
+	h.wg.Add(1)
+
 	_, err := url.Parse(h.Endpoint)
 	if err != nil {
 		return fmt.Errorf("invalid URL \"%s\"", h.Endpoint)
 	}
 
-	if h.client == nil {
-		//tr := &http.Transport{
-		//	ResponseHeaderTimeout: time.Duration(3 * time.Second),
-		//}
-		//
-		//hc := &http.Client{
-		//	Transport: tr,
-		//	Timeout:   time.Duration(4 * time.Second),
-		//}
-
-		h.client = gitlab.NewClient(nil, h.Token)
-		h.client.SetBaseURL(h.Endpoint)
-	}
+	h.client = gitlab.NewClient(nil, h.Token)
+	h.client.SetBaseURL(h.Endpoint)
 
 	ps, resp, err := h.client.Projects.ListProjects(&gitlab.ListProjectsOptions{})
 	if err != nil {
 		return fmt.Errorf("unable to perform HTTP client GET on \"%s\": %s", h.Endpoint, err)
 	}
-	defer resp.Body.Close()
 
-	projects := make(map[int]string)
+	h.projects = make(map[int]string)
 	for _, p := range ps {
-		projects[p.ID] = p.Name
+		h.projects[p.ID] = p.Name
 	}
-
+	resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
 		return fmt.Errorf("http status ok not met")
 	}
 
-getmr:
-	mrs, rs, err := h.client.MergeRequests.ListMergeRequests(&gitlab.ListMergeRequestsOptions{
-		Scope: gitlab.String("all"),
-		ListOptions: gitlab.ListOptions{PerPage: 100, Page: h.currentPage},
-	})
-
-	if err != nil {
-		return fmt.Errorf("unable to list merge requests, %+v", err)
-	}
-
-	fmt.Print(len(mrs))
-	for _, mr := range mrs {
-		tmpFields := map[string]interface{}{
-			"upvotes":   mr.Upvotes,
-			"downvotes": mr.Downvotes,
-			"changes":   mr.ChangesCount,
-			"changes_two": len(mr.Changes),
-			"wip": mr.WorkInProgress,
-		}
-
-		tmpTags := map[string]string{
-			"author": mr.Author.Name,
-			"username": mr.Author.Username,
-			"assignee": mr.Assignee.Name,
-			"project": projects[mr.ProjectID],
-			"state": mr.State,
-		}
-		acc.AddFields("merge_requests", tmpFields, tmpTags, *mr.CreatedAt)
-	}
-	rs.Body.Close()
-	if len(mrs) == 100 {
-		h.currentPage += 1
-		goto getmr
-	}
-
-	rs.Body.Close()
-
+	go h.fetchMergeRequests(acc)
 	return nil
+}
+
+func (h *Gitlab) fetchMergeRequests(acc telegraf.Accumulator) {
+	defer h.wg.Done()
+	pp := 100
+	page := 0
+
+	for {
+		mrs, rs, err := h.client.MergeRequests.ListMergeRequests(&gitlab.ListMergeRequestsOptions{
+			Scope:       gitlab.String("all"),
+			ListOptions: gitlab.ListOptions{PerPage: pp, Page: page},
+		})
+
+		if err != nil {
+			acc.AddError(fmt.Errorf("unable to list merge requests, %+v", err))
+			break
+		}
+
+		for _, mr := range mrs {
+			tmpFields := map[string]interface{}{
+				"upvotes":     mr.Upvotes,
+				"downvotes":   mr.Downvotes,
+				"changes":     mr.ChangesCount,
+				"updated_at":  mr.UpdatedAt,
+				"notes_count": mr.UserNotesCount,
+				"wip":         mr.WorkInProgress,
+			}
+
+			tmpTags := map[string]string{
+				"merge_status": mr.MergeStatus,
+				"author":       mr.Author.Name,
+				"username":     mr.Author.Username,
+				"assignee":     mr.Assignee.Name,
+				"project":      h.projects[mr.ProjectID],
+				"state":        mr.State,
+			}
+			acc.AddFields("merge_requests", tmpFields, tmpTags, *mr.CreatedAt)
+		}
+		rs.Body.Close()
+		page += 1
+		if len(mrs) < pp {
+			break
+		}
+	}
 }
 
 func init() {
